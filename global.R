68c90262fafa65cdd84d63b4ccb764f7bcdc4ac0
Laureen Omare <gesarelo@gmail.com>
2021-07-15 21:48:04 +0300

    revert and see?

--- global.R
+++ global.R
@@ -1,44 +1,46 @@
 library(shiny)
 library(randomForest)
-# library(caret)
-library(xgboost)
 library(dplyr)
 library(RSQLite)
 library(shinythemes)
 library(lubridate)
 library(RMariaDB)
-library(shinyjs)
 
-#source("alter_database.R")
 # Recall, Precision, Threshold
-cutoffs <- readRDS('./cutoffs_2021-07-14.rds')
+THRESH_75 <- list('22.7%', '75%', .446)
+THRESH_50 <- list('44.7%', '50%', .232)
+THRESH_25 <- list('67.5%', '25%', .068)
 
-mod <- readRDS('homabay_model_20210713.rds')
-dat <- readRDS('./hts_homabay_imputed_0525.rds')
-dat <- dat$sparse$train
-dat <- dat %>%
-  select(-Facility.Name, -Longitude, -Latitude, -Sitecode)
-factor_vars <- names(dat)[ sapply(dat, is.factor) ]
-factor_vars <- factor_vars[factor_vars!="FinalTestResult"]
-dat_unique <- apply(dat[, factor_vars], 2, function(x)unique(x[!is.na(x)]))
+mod <- readRDS('rf_homabay_20210210.rds')
+dat <- readRDS('training_homabay_20210210.rds') 
+# sc_info <- dat %>% select(2:76) %>% unique()
+# Adding info for Rangwe Sub-Distrcit Hospital
+# rangwe <- readRDS('../../../../../facilities_homabay_5km.rds') %>%
+#   filter(Facility.Name == "Rangwe Sub-District Hospital") %>% 
+#   mutate(Sitecode = 14036,
+#          FacilityType = "Hospital") %>%
+#   select(names(sc_info))
+# sc_info <- rbind(sc_info, rangwe)
+# saveRDS(sc_info, './sc_info_homabay.rds')
+sc_info <- readRDS('./sc_info_homabay.rds')
 
-facilities <- readRDS('./facilities_pca_20210713.rds') %>%
-  select(-Latitude, -Longitude)
+# Read in facility index
+facilities <- read.csv('./Facility Export Material List.csv', stringsAsFactors = FALSE) %>%
+  select(1:2) %>%
+  # rbind(., data.frame(Code = "99999", Name = "Other")) %>%
+  filter(Code %in% c(unique(dat$Sitecode), 14036))
 
+# Read in Population Type
+population <- read.csv('./KeyPopulation.csv', stringsAsFactors = FALSE) %>%
+  select(1:2) %>%
+  filter(PopCode %in% unique(dat$KeyPopulationType))
 
+# Read in Enrty Point
+entrypoint <- read.csv('./EntryPoint.csv', stringsAsFactors = FALSE) %>%
+  select(1:2) %>%
+  filter(ID %in% unique(dat$EntryPoint))
 
-encodeXGBoost <- function(dataset){
-
-  # Need to one-hot encode all the factor variables
-  ohe_features <- names(dataset)[ sapply(dataset, is.factor) ]
-  
-  dmy <- dummyVars("~ Gender +  KeyPopulationType + MaritalStatus + PatientDisabled + EverTestedForHIV +
-                   ClientTestedAs + EntryPoint + TestingStrategy + TBScreening + ClientSelfTested +
-                   month_of_test + dayofweek",
-                   data = dataset)
-  ohe <- data.frame(predict(dmy, newdata = dataset))
-  dataset <- cbind(dataset, ohe)
-  
-  dataset[, !(names(dataset) %in% ohe_features)]
-  
-}
+# Read in Testing Strategy
+testingstrategy <- read.csv('./Testing Strategy.csv', stringsAsFactors = FALSE) %>%
+  select(1:2) %>%
+  filter(ID %in% unique(dat$TestingStrategy))
--- ui.R
+++ ui.R
@@ -1,11 +1,9 @@
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-    theme = shinythemes::shinytheme("spacelab"),
-    shinyjs::useShinyjs(),
-                  
+shinyUI(fluidPage(theme = shinythemes::shinytheme("spacelab"),
+
     # Application title
     titlePanel("Homa Bay County HTS Client Assessment"),
-    
+
     # Sidebar with a slider input for number of bins
     "Welcome to the National HTS Client Assesment Portal. 
           This platform is intended to help you make a better decision on
@@ -14,61 +12,58 @@ shinyUI(fluidPage(
     "Kindly proceed to fill in the bio details of your patient to recieve
     the prediction.",
     br(),
-    br(),
-
-    # Sidebar with a slider input for number of bins
+    
     sidebarLayout(
         sidebarPanel(
-            textInput("HTSNumber",
-                        "HTS Number"),
-            selectInput("facilityname",
-                        "Facility Name",
-                        choices = c("", facilities$Facility.Name),
-                        selected = NULL),
-            numericInput("ageattest",
-                         "Client's Age",
-                         value = 0,
-                         min = 0,
-                         max = 100),
-            selectInput("gender",
-                        "Gender",
-                        choices = c("", dat_unique$Gender),
-                        selected = NULL),
-            selectInput("maritalstatus",
-                        "Marital Status",
-                        choices = c("", dat_unique$MaritalStatus, "Single"),
-                        selected = NULL),
-            selectInput("KPtype",
-                        "Population type:",
-                        choices = c("", dat_unique$KeyPopulationType, "PWID"),
-                        selected = NULL),
-            selectInput("patientdisabled",
-                        "Patient Disabled",
-                        choices = c("", dat_unique$PatientDisabled),
-                        selected = NULL),
+                  selectInput("sitecode",
+                              "Facility Name",
+                              choices = c("", facilities$Name),
+                              selected = NULL),
+                    numericInput("ageattest",
+                                 "Client's Age",
+                                 value = 0,
+                                 min = 0,
+                                 max = 100),
+                  selectInput("gender",
+                              "Gender",
+                              choices = c("", as.character(unique(dat$Gender))),
+                              selected = NULL),
+                    selectInput("maritalstatus",
+                                "Marital Status",
+                                choices = c("", as.character(unique(dat$MaritalStatus))),
+                                selected = NULL),
+                    
+                        selectInput("KPtype",
+                                    "Population type:",
+                                    choices = c("", population$PopulationType),
+                                    selected = NULL),
+            # selectInput("patientdisabled",
+            #             "Client Disability Status",
+            #             choices = c("", as.character(unique(dat$PatientDisabled))),
+            #             selected = NULL),
             selectInput("clienttestedas",
                         "Client Testing As",
-                        choices = c("", dat_unique$ClientTestedAs),
+                        choices = c("", as.character(unique(dat$ClientTestedAs))),
                         selected = NULL),
-            selectInput("entrypoint",
-                        "Entry Point",
-                        choices = c("", dat_unique$EntryPoint),
-                        selected = NULL),
+            # selectInput("entrypoint",
+            #             "Entry Point",
+            #             choices = c("", as.character(unique(dat$EntryPoint))),
+            #             selected = NULL),
             selectInput("testingstrategy",
                         "Testing Strategy",
-                        choices = c("", dat_unique$TestingStrategy, "NP", "HP", "PNS"),
+                        choices = c("", testingstrategy$Name),
                         selected = NULL),
             selectInput("tbscreening",
                         "TB Screening Results",
-                        choices = c("", dat_unique$TBScreening),
+                        choices = c("", as.character(unique(dat$TBScreening))),
                         selected = NULL),
             selectInput("clientselftested",
                         "Ever had an HIV Self Test",
-                        choices = c("", dat_unique$ClientSelfTested),
+                        choices = c("", as.character(unique(dat$ClientSelfTested))),
                         selected = NULL),
             selectInput("evertested",
                         "Ever Tested for HIV by a HealthWorker",
-                        choices = c("", dat_unique$EverTestedForHIV),
+                        choices = c("", as.character(unique(dat$EverTestedForHIV))),
                         selected = NULL),
             conditionalPanel(
                 condition = "input.evertested == 'Yes'",
@@ -87,9 +82,12 @@ shinyUI(fluidPage(
             verbatimTextOutput("predText"),
             br(),
             br(),
-            actionButton('recPred', 'Save Prediction',class="btn-primary"),
+            actionButton('recPred', 'Save Prediction', class = "btn-primary"),
             br(),
-            br()
+            br(),
+            br(),
+            br(),
+            actionButton('recResult', 'Record Test Result',class="btn-primary")
         )
-    )
+        )
 ))
