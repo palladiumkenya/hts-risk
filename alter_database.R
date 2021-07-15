fd5eb23bc390d059462e2923344461eb04811bb8
Laureen Omare <gesarelo@gmail.com>
2021-07-15 15:22:41 +0300

    Changing Model

---
+++ alter_database.R
@@ -0,0 +1,14 @@
+library(RMariaDB)
+
+dbConfig <- config::get("database")
+conn <- dbConnect(
+    RMariaDB::MariaDB(),
+    dbname = dbConfig$dbname,
+    host = dbConfig$host,
+    port = dbConfig$port,
+    username = dbConfig$username,
+    password = dbConfig$password
+)
+dbExecute(conn, "ALTER TABLE homabayhts ADD HTSNumber varchar(25);
+          UPDATE `hts_risk`.`homabayaccess` SET `usernames` = 'Test' WHERE `usernames` = 'ERIC';")
+dbDisconnect(conn)
--- ui.R
+++ ui.R
@@ -1,9 +1,11 @@
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(theme = shinythemes::shinytheme("spacelab"),
-
+shinyUI(fluidPage(
+    theme = shinythemes::shinytheme("spacelab"),
+    shinyjs::useShinyjs(),
+                  
     # Application title
     titlePanel("Homa Bay County HTS Client Assessment"),
-
+    
     # Sidebar with a slider input for number of bins
     "Welcome to the National HTS Client Assesment Portal. 
           This platform is intended to help you make a better decision on
@@ -12,58 +14,61 @@ shinyUI(fluidPage(theme = shinythemes::shinytheme("spacelab"),
     "Kindly proceed to fill in the bio details of your patient to recieve
     the prediction.",
     br(),
-    
+    br(),
+
+    # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-                  selectInput("sitecode",
-                              "Facility Name",
-                              choices = c("", facilities$Name),
-                              selected = NULL),
-                    numericInput("ageattest",
-                                 "Client's Age",
-                                 value = 0,
-                                 min = 0,
-                                 max = 100),
-                  selectInput("gender",
-                              "Gender",
-                              choices = c("", as.character(unique(dat$Gender))),
-                              selected = NULL),
-                    selectInput("maritalstatus",
-                                "Marital Status",
-                                choices = c("", as.character(unique(dat$MaritalStatus))),
-                                selected = NULL),
-                    
-                        selectInput("KPtype",
-                                    "Population type:",
-                                    choices = c("", population$PopulationType),
-                                    selected = NULL),
-            # selectInput("patientdisabled",
-            #             "Client Disability Status",
-            #             choices = c("", as.character(unique(dat$PatientDisabled))),
-            #             selected = NULL),
+            textInput("HTSNumber",
+                        "HTS Number"),
+            selectInput("facilityname",
+                        "Facility Name",
+                        choices = c("", facilities$Facility.Name),
+                        selected = NULL),
+            numericInput("ageattest",
+                         "Client's Age",
+                         value = 0,
+                         min = 0,
+                         max = 100),
+            selectInput("gender",
+                        "Gender",
+                        choices = c("", dat_unique$Gender),
+                        selected = NULL),
+            selectInput("maritalstatus",
+                        "Marital Status",
+                        choices = c("", dat_unique$MaritalStatus, "Single"),
+                        selected = NULL),
+            selectInput("KPtype",
+                        "Population type:",
+                        choices = c("", dat_unique$KeyPopulationType, "PWID"),
+                        selected = NULL),
+            selectInput("patientdisabled",
+                        "Patient Disabled",
+                        choices = c("", dat_unique$PatientDisabled),
+                        selected = NULL),
             selectInput("clienttestedas",
                         "Client Testing As",
-                        choices = c("", as.character(unique(dat$ClientTestedAs))),
+                        choices = c("", dat_unique$ClientTestedAs),
                         selected = NULL),
-            # selectInput("entrypoint",
-            #             "Entry Point",
-            #             choices = c("", as.character(unique(dat$EntryPoint))),
-            #             selected = NULL),
+            selectInput("entrypoint",
+                        "Entry Point",
+                        choices = c("", dat_unique$EntryPoint),
+                        selected = NULL),
             selectInput("testingstrategy",
                         "Testing Strategy",
-                        choices = c("", testingstrategy$Name),
+                        choices = c("", dat_unique$TestingStrategy, "NP", "HP", "PNS"),
                         selected = NULL),
             selectInput("tbscreening",
                         "TB Screening Results",
-                        choices = c("", as.character(unique(dat$TBScreening))),
+                        choices = c("", dat_unique$TBScreening),
                         selected = NULL),
             selectInput("clientselftested",
                         "Ever had an HIV Self Test",
-                        choices = c("", as.character(unique(dat$ClientSelfTested))),
+                        choices = c("", dat_unique$ClientSelfTested),
                         selected = NULL),
             selectInput("evertested",
                         "Ever Tested for HIV by a HealthWorker",
-                        choices = c("", as.character(unique(dat$EverTestedForHIV))),
+                        choices = c("", dat_unique$EverTestedForHIV),
                         selected = NULL),
             conditionalPanel(
                 condition = "input.evertested == 'Yes'",
@@ -82,12 +87,9 @@ shinyUI(fluidPage(theme = shinythemes::shinytheme("spacelab"),
             verbatimTextOutput("predText"),
             br(),
             br(),
-            actionButton('recPred', 'Save Prediction', class = "btn-primary"),
+            actionButton('recPred', 'Save Prediction',class="btn-primary"),
             br(),
-            br(),
-            br(),
-            br(),
-            actionButton('recResult', 'Record Test Result',class="btn-primary")
+            br()
         )
-        )
+    )
 ))
